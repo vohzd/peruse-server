3cda9d4849671debdac950f8dbc0def958249ecf
Ben <allobon@gmail.com>
2020-01-13 23:01:25 +0000

    tidy

--- server/index.js
+++ server/index.js
@@ -1,41 +1,30 @@
 /*
  * DEPS
  */
-const express = require("express");
-const port = 1337;
-const app = express();
-//const io                            = require("socket.io")(http);
-
-/*
- *   MIDDLEWARE
- */
-
-//require("./middleware/auth.js");
-//require("./middleware/logger.js")(app);
+const express                       = require("express");
+const port                          = 1337;
+const app                           = express();
+const http                          = require("http").Server(app);
+const io                            = require("socket.io")(http);
 
 /*
  *   CONFIG
  */
 require("../config/server.js")(app);
-//require("./config/db.js");
 
 /*
  *   ROUTES
  */
 
-///const userRoutes                     = require("./routes/user/index.js");
-const routes = require("./routes/routes.js");
-app.use("/libraries", express.static(__dirname + "/libraries"));
+const routes                        = require("./routes/routes.js")(io);
 
+app.use("/libraries", express.static(__dirname + "/libraries"));
 app.use("/", routes);
 
-// serve js lib
-//app.use("/", trackRoutes);
 
 /*
  *   START SERVER
  */
-
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`working on ${port}`);
 });
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,25 +2,47 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get(
-  "/sites",
-  async (req, res, next) => {
-    setTimeout(async () => {
-      return await res.json({ sites: "none" });
-    }, 1500);
-  },
-  (err, req, res, next) => {
-    return res.json(err);
-  }
-);
+const routes = (io) => {
 
-router.post("/listen", async (req, res) => {
-  console.log("you rang?");
-  console.log(req.body);
-  return res.json({
-    message:
-      "why do i always feel like, someone is watching me? https://www.youtube.com/watch?v=qQe_w-A9Y50"
+  io.on("connection", (socket) => {
+    console.log("connection")
+    //socket.emit("user-connected", { "status": "yes" });
+
+    router.post("/listen", async (req, res) => {
+      console.log("you are here...")
+      socket.emit("user-action", { "status": "yes" });
+
+      return res.json({
+        message:
+          "why do i always feel like, someone is watching me? https://www.youtube.com/watch?v=qQe_w-A9Y50"
+      });
+    });
+
+    socket.on("connection", (something) => {
+      console.log("something happened...")
+    });
+
+
+
   });
-});
 
-module.exports = router;
+  console.log(" you are here")
+
+  router.get(
+    "/sites",
+    async (req, res, next) => {
+      setTimeout(async () => {
+        return await res.json({ sites: "none" });
+      }, 1500);
+    },
+    (err, req, res, next) => {
+      return res.json(err);
+    }
+  );
+
+
+
+  return router;
+}
+
+module.exports = routes;
